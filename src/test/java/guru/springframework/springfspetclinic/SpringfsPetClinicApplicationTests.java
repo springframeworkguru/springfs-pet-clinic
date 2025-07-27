07325e5c5b5f53f761606bd8bef86f02e61bc36a
John Thompson <john@springframework.guru>
2025-07-27 09:55:12 -0400

    initial project add

---
+++ src/test/java/guru/springframework/springfspetclinic/SpringfsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springfspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
