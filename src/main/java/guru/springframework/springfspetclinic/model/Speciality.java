4c83be2068019017d5c5bb4bf7db646fa7d9559a
John Thompson <john@springframework.guru>
2025-08-02 09:46:10 -0400

    Add Speciality class with entity and table annotations

---
+++ src/main/java/guru/springframework/springfspetclinic/model/Speciality.java
@@ -0,0 +1,10 @@
+package guru.springframework.springfspetclinic.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
+public class Speciality extends NamedEntity {
+
+}
